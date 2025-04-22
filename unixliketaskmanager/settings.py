b6e9f4058707e83f240d06ed660a505f759dfb22
jztchl <kmvishnu625@gmail.com>
2025-04-22 20:39:38 +0530

    static file handling'

--- unixliketaskmanager/settings.py
+++ unixliketaskmanager/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -154,8 +154,9 @@ TEMPLATES = [
         },
     },
 ]
-
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Or wherever you want them dumped
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field

6296b5c9c8d54bf92c751c2ba79380a2bdfff7bc
jztchl <kmvishnu625@gmail.com>
2025-04-22 21:38:38 +0530

    cors

--- unixliketaskmanager/settings.py
+++ unixliketaskmanager/settings.py
@@ -37,7 +37,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
         
     "http://127.0.0.1:8000",   
-    "http://localhost:8000",     
+    "http://localhost:8000",
+    "https://unixtask.koyeb.app"    
 ]
 
 
